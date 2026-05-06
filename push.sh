eec37b1c5a50fee45365eb3fac3c0cd1250228df
Ubuntu <ubuntu@ip-172-31-15-232.us-west-1.compute.internal>
2026-05-06 12:44:06 +0000

    Update: 2026-05-06 12:44:06

--- push.sh
+++ push.sh
@@ -7,6 +7,6 @@ echo "Committing changes..."
 git commit -m "Update: $(date '+%Y-%m-%d %H:%M:%S')"
 
 echo "Pushing to remote..."
-git push -u origin main
+git push -u origin master
 
 echo "Done! 100% complete"
